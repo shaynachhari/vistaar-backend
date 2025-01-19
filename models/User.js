b2902520cb36c0167799ffc881b3347cb1f2b89c
shayna chhari <shaynachhari@gmail.com>
2025-01-19 21:02:01 +0530

    upload dir created

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  googleId: { type: String, unique: true },
+  googleId: { type: String },
   role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
